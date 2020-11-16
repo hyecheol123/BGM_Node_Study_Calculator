12f2a774af2a984e8a3870ea6f9ab586a64e8f01
Hyecheol (Jerry) Jang <hyecheol.jang@wisc.edu>
2020-11-16 21:40:21 +0900

    Fix HTML Template + Client Javascript start

---
+++ web/calc.js
@@ -0,0 +1,25 @@
+/**
+ * Javascript file to handle events and get calculation results from Node.js Server
+ *
+ * @author Hyecheol (Jerry) Jang
+ */
+
+function backspace() {
+  // TODO
+}
+
+function clear() {
+  // TODO
+}
+
+function result() {
+  // TODO
+}
+
+function input(button) {
+  // TODO
+}
+
+function enterDetect() {
+  // TODO
+}
