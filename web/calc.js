8f09e4c5fc1c837d19bcebdb3018d584c19de79d
Hyecheol (Jerry) Jang <hyecheol.jang@wisc.edu>
2020-11-19 23:11:13 +0900

    Client - Javascript to handle button input

--- web/calc.js
+++ web/calc.js
@@ -3,23 +3,56 @@
  *
  * @author Hyecheol (Jerry) Jang
  */
+let displayResult = false; // indicate display does not showing result
 
+/**
+ * Remove most recently added character
+ */
 function backspace() {
-  // TODO
+  if(displayResult) {
+    clear();
+  } else {
+    // TODO
+  }
 }
 
+/**
+ * Remove all characters & allow type-in
+ */
 function clear() {
   // TODO
+  displayResult = false;
 }
 
+/**
+ * Check for input validity (only allow numbers and selected operators)
+ * and get calculated result from Node server
+ *
+ * After result displayed, this function will block keyboard type-in before the display cleared
+ */
 function result() {
   // TODO
+  displayResult = true;
 }
 
+/**
+ * Add a character associated with each button
+ *
+ * @param button character associated with the button
+ */
 function input(button) {
-  // TODO
+  // When display is showing result, we need to clear the display before we start new calculation
+  if(displayResult) {
+    clear();
+  }
+  // Add character assigned to clicked button at the end of existing string
+  document.getElementById("display").insertAdjacentText("beforeend", button);
 }
 
+/**
+ * Check whether user pressed "Enter"
+ * if "Enter" pressed, run result() function to get result
+ */
 function enterDetect() {
   // TODO
 }
